ea9a50bcc04c8bf92502cea98d539a5f6512723f
aaff20041995-rgb <aaff20041995@gmail.com>
2026-08-17 22:30:00 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const C='53b-v2',A=['./','./index.html','./manifest.webmanifest','./icons/icon-180.png','./icons/icon-192.png','./icons/icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(x=>x||fetch(e.request).then(r=>{let q=r.clone();caches.open(C).then(c=>c.put(e.request,q));return r}).catch(()=>caches.match('./index.html'))))});
